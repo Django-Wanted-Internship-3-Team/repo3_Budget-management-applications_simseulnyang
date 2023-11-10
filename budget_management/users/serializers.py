1136448d6cfc865985cdb26d5d4d96923239c304
simseulnyang <happysseul627@gmail.com>
2023-11-10 12:48:38 +0900

    :sparkles:Feature : 사용자 로그인 API 작성 #4
    - LoginSerializer 작성
    - api/users/login 에 대한 view 인터페이스 작성

--- budget_management/users/serializers.py
+++ budget_management/users/serializers.py
@@ -1,4 +1,6 @@
+from django.contrib.auth import authenticate
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from budget_management.users.models import User
 
@@ -20,4 +22,35 @@ class UserSignupSerializer(serializers.ModelSerializer):
             username=validated_data["username"],
             password=validated_data["password"],
         )
+        return user
+
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    token = serializers.SerializerMethodField(read_only=True)
+
+    def get_token(self, user):
+        if user is not None:
+            refresh = TokenObtainPairSerializer.get_token(user)
+            refresh["username"] = user.username
+            data = {
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+            }
+            return data
+        return None
+
+    def validate(self, data):
+        user = authenticate(username=data["username"], password=data["password"])
+
+        if user is None:
+            raise serializers.ValidationError("Username or Password is Incorrect")
+        return data
+
+    def create(self, validated_data):
+        user = authenticate(username=validated_data["username"], password=validated_data["password"])
+        if user is not None:
+            user.is_active = True
+            user.save()
         return user
